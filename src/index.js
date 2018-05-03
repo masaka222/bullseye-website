54331f23ee0abe768c9218d9d0250cba697fff7f
Vasilis Georgoudis <masakaftw@hotmail.com>
2018-05-04 00:38:07 +0300

    Deleted unecessary files plus new favicon.ico

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<p>Bullseye</p>, document.getElementById('root'));
 registerServiceWorker();
