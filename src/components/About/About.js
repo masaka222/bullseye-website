7add83303edfbfaf40f58de077882bded626a375
Vasilis Georgoudis <masakaftw@hotmail.com>
2018-05-09 02:50:43 +0300

    About Grid left Column

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import HeadingSecondary from '../HeadingSecondary/HeadingSecondary';
+import Grid from '../Grid/Grid';
 
 const About = (props) => {
         return (
             <div>
                 <section className="section-about">
                     <HeadingSecondary aboutHeading = {props.aboutHeading}/>
+                    <Grid/>
                </section>
             </div>
         );
---
+++ src/components/Grid/Grid.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Grid = () => {
+        return (
+            <div className="row">
+                <div className="col-1-of-2">
+                    <h3 className="heading-tertiary u-margin-bottom-medium">Our love for a good burger brought us in Glenside</h3>
+                    <p className="paragraph">
+                        Lorem ipsum dolor sit amet consectetur adipiscing elit sodales, 
+                        conubia lectus rhoncus erat lacus elementum nulla, bibendum facilisis 
+                        viverra per ut velit diam. Eros mattis sodales eget justo lobortis urna 
+                        scelerisque ultrices per vivamus, laoreet blandit viverra integer lectus 
+                        non risus magnis enim, magna egestas facilisis fusce id hendrerit eleifend dapibus faucibus. 
+                        Nascetur mattis eros mus vulputate id hendrerit suscipit magnis, euismod orci litora platea 
+                        ut sollicitudin.
+                    </p>
+                </div>
+                <div className="col-1-of-2">
+                    Col 1 of 2
+                </div>
+            </div>
+        );
+}
+
+export default Grid;
