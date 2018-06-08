17a5b4d5769859be7c0421a4ce2d0960f582b48c
Vasilis Georgoudis <masakaftw@hotmail.com>
2018-06-09 02:24:54 +0300

    Navigation Secondary Part 3

--- src/components/NavigationSecondary/NavigationSecondary.js
+++ src/components/NavigationSecondary/NavigationSecondary.js
@@ -4,15 +4,17 @@ const NavigationSecondary = () => {
         return (
             <div className="navigation__fullscreen">
                 <input type="checkbox" className="navigation__checkbox" id="navi-toggle"/>
-                <label for="navi-toggle" className="navigation__button">MENU</label>
+                <label for="navi-toggle" className="navigation__button">
+                    <span className="navigation__icon">&nbsp;</span>
+                </label>
                 <div className="navigation__background">&nbsp;</div>
 
                 <nav className = "navigation__nav">
                     <ul className = "navigation__list">
-                        <li className="navigation__item"><a href = "#section-contact" className="navigation__link">About</a></li>
-                        <li className="navigation__item"><a href = "#section-reviews" className="navigation__link">Menu</a></li>
-                        <li className="navigation__item"><a href = "#section-menu" className="navigation__link">Reviews</a></li>
-                        <li className="navigation__item"><a href = "#section-about" className="navigation__link">Contact</a></li>
+                        <li className="navigation__item"><a href = "#section-about" className="navigation__link">About</a></li>
+                        <li className="navigation__item"><a href = "#section-menu" className="navigation__link">Menu</a></li>
+                        <li className="navigation__item"><a href = "#section-reviews" className="navigation__link">Reviews</a></li>
+                        <li className="navigation__item"><a href = "#section-contact" className="navigation__link">Contact</a></li>
                     </ul>
                 </nav>
             </div>
