5aa697a3495ba95c9a345674741bb75ad18c7c34
Vasilis Georgoudis <masakaftw@hotmail.com>
2018-06-23 13:17:30 +0300

    Bug Fix for HeadingSecondary Animation

--- src/components/HeadingSecondary/HeadingSecondary.js
+++ src/components/HeadingSecondary/HeadingSecondary.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const HeadingSecondary = (props) => {
         return (
-            <div className = {`u-center-text ${props.headingMarginBottom}`}>
+            <div style = {{paddingTop: '13rem', marginTop:'-13rem'}} className = {`u-center-text ${props.headingMarginBottom}`} id = {props.headingId}>
             {/*The marginTop actually hides the paddingTop, and this hack is done in order the Navigation
             to stop some pixels before the actual heading*/}
-                <h2 style = {{paddingTop: '13rem', marginTop:'-13rem'}} className="heading-secondary"><span id = {props.headingId}></span>{props.headingSecondary}</h2>
+                <h2 className="heading-secondary">{props.headingSecondary}</h2>
             </div>
         );
 }
