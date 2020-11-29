801eb6f3dd2bd1d1520db7e51494c0666fc66f3b
Vasilis Georgoudis <masakaftw@hotmail.com>
2020-11-29 20:30:07 +0200

    Order online button added

--- src/components/HeadingPrimary/HeadingPrimary.js
+++ src/components/HeadingPrimary/HeadingPrimary.js
@@ -7,6 +7,8 @@ const HeadingPrimary = (props) => {
                     <span className = "heading-primary-main">{props.headerHeading}</span>
                     <span className = "heading-secondary-main">&#x02605;{props.headerSubHeading}&#x02605;</span>
                 </h1>
+
+                <a href='https://www.bullseyeburgerhousetogo.com' className='btn btn-white'>Order Online</a>
             </div>
         );
 }
