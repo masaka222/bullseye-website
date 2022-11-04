89609017910e687382ffdc1897e838e7a845d591
Vasilis Georgoudis <masakaftw@hotmail.com>
2022-11-04 11:07:58 -0400

    chore: update online ordering link

--- src/components/HeadingPrimary/HeadingPrimary.js
+++ src/components/HeadingPrimary/HeadingPrimary.js
@@ -1,16 +1,26 @@
 import React from 'react';
 
 const HeadingPrimary = (props) => {
-        return (
-            <div className="header__text-box">
-                <h1 className="heading-primary">
-                    <span className = "heading-primary-main">{props.headerHeading}</span>
-                    <span className = "heading-secondary-main">&#x02605;{props.headerSubHeading}&#x02605;</span>
-                </h1>
-
-                <a href='https://onlineordering.rmpos.com/Order/?wci=oFh1t6Ok' className='btn btn-white'>Order Online</a>
-            </div>
-        );
+  return (
+    <div className="header__text-box">
+      <h1 className="heading-primary">
+        <span className = "heading-primary-main">
+          {props.headerHeading}
+        </span>
+        <span className="heading-secondary-main">
+          &#x02605;{props.headerSubHeading}&#x02605;
+        </span>
+      </h1>
+      <a 
+        className='btn btn-white' 
+        target="_blank"
+        rel="noopener noreferrer"
+        href='https://slicelife.com/restaurants/pa/glenside/19038/bullseye-burger-house-glenside/menu'
+      >
+        Order Online
+      </a>
+    </div>
+  );
 }
 
 export default HeadingPrimary;
