0b736bfd713db7266b3f59ab9b6a83a90f1441e6
Vasilis Georgoudis <masakaftw@hotmail.com>
2022-11-05 08:28:32 -0400

    feat: prevent index.html from caching

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './icofont.css';
 import './styles.css';
-import registerServiceWorker from './registerServiceWorker';
+import { unregister } from './registerServiceWorker';
 import App from './components/App/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+unregister();
